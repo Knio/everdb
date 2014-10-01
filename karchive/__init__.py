ffb02de5edd3520e895f3310c4d7b6acfb5edc49
Tom Flanagan <tom@zkpq.ca>
2014-09-30 18:45:20 -0700

    add setup.py

--- karchive/__init__.py
+++ karchive/__init__.py
@@ -1,3 +1,6 @@
+from _version import *
+version = __version__
+
 from .fileblockdevice   import FileBlockDevice
 from .database          import Database
 
---
+++ setup.py
@@ -0,0 +1,67 @@
+__license__ = '''
+This file is part of karchive.
+
+karchive is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+karchive is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with karchive.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+# pylint: disable=bad-whitespace
+
+# http://guide.python-distribute.org/creation.html
+
+from setuptools import setup
+
+import imp
+_version = imp.load_source("karchive._version", "karchive/_version.py")
+
+long_description = open('README.md').read()
+
+# PyPI only supports (an old version of?) ReST.
+# Doesn't seem to be compatable with Pandoc. Shame on you.
+
+# try:
+#   import pypandoc
+#   long_description = pypandoc.convert(
+#     long_description, 'rst', format='markdown_github')
+# except:
+#   import traceback
+#   traceback.print_exc()
+
+setup(
+  name    = 'karchive',
+  version = _version.__version__,
+  author  = 'Tom Flanagan and knivey',
+  author_email = 'tom@zkpq.ca',
+  license = 'LICENSE.txt',
+  url     = 'http://github.com/Knio/karchive/',
+
+  description      = 'karchive is a fast embedded database for python',
+  long_description = long_description,
+  keywords         = 'python database emedded',
+
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Database',
+    'Topic :: Database :: Database Engines/Servers',
+  ],
+
+  packages = ['karchive'],
+  include_package_data = True,
+)
