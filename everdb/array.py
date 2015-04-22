5bac7e06661828a2cafe2787e284943056eb8f56
Tom Flanagan <tom@zkpq.ca>
2015-04-21 22:18:18 -0700

    fix array

--- everdb/array.py
+++ everdb/array.py
@@ -8,13 +8,11 @@ import struct
 
 from .page import Page, Field
 from .page import SMALL_PAGE, REGULAR_PAGE
-from .blob import BLOCK, OFFSET
 from .page import ZERO_BLOCK, BLOCK_MASK, BLOCK_SIZE
 
 class Array(Page):
   format    = Field('c')
   length    = Field('Q')
-  capacity  = Field('Q')
 
   def __init__(self, host, root, format, new):
     self.format = format.encode('ascii')
@@ -24,9 +22,15 @@ class Array(Page):
 
   def init_root(self):
     MAX_SMALL = (BLOCK_SIZE - self._header_size) // self.item_size
-    self.capacity = MAX_SMALL
     self.length = 0
     super(Array, self).init_root()
+
+  @property
+  def capacity(self):
+    if self.type == SMALL_PAGE:
+      MAX_SMALL = (BLOCK_SIZE - self._header_size)
+      return MAX_SMALL // self.item_size
+    return self.num_blocks * self.items_per_block
 
   def __len__(self):
     return self.length
@@ -49,7 +53,7 @@ class Array(Page):
     else:
       b = self.get_block(j)
 
-    # TODO cache this
+    # TODO SLOW cache this
     return b.cast(self.format.decode('ascii'))[k]
 
   def __setitem__(self, i, v):
@@ -67,7 +71,7 @@ class Array(Page):
     else:
       b = self.get_block(j)
 
-    # TODO cache this
+    # TODO SLOW cache this
     b.cast(self.format.decode('ascii'))[k] = v
 
   def getslice(self, i):
@@ -86,7 +90,10 @@ class Array(Page):
     self[l] = v
     # ensure 1 extra slot
     if self.capacity < l + 2:
-      self.resize(l + 2)
+      if self.num_blocks == 0:
+        self.make_regular()
+      else:
+        self.allocate(self.num_blocks + 1)
 
   def pop(self):
     if not self.length:
@@ -94,79 +101,72 @@ class Array(Page):
     l = self.length
     j = l - 1
     x = self[j]
+    self[j] = 0
     self.length = j
-    if self.capacity > l + 2:
-      self.resize(l + 2)
+
+    if self.num_blocks == 1:
+      if (BLOCK_SIZE - self._header_size) // self.item_size > l:
+        self.make_small()
+
+    elif self.num_blocks > 1:
+      if (self.num_blocks - 1) * self.items_per_block > l:
+        self.allocate(self.num_blocks - 1)
+
     return x
 
-  def resize(self, capacity):
+  def resize(self, length):
     '''
     Resizes the array to a given size (in elements, not bytes)
     '''
     # requested size fits in a small block
     # handles both grow and shrink operation
-    length = capacity * self.item_size
-
-    MAX_SMALL = (BLOCK_SIZE - self._header_size)
+    MAX_SMALL = (BLOCK_SIZE - self._header_size) // self.item_size
     if length <= MAX_SMALL:
+      s = slice(length, MAX_SMALL)
       if self.type == REGULAR_PAGE:
         # copy data to root in small block
         data = bytes(self.get_block(0)[0:length])
         # free data + page blocks,
         self.allocate(0)
         self.host[self.root][0:length] = data
-      else:
-        # already a small block
-        # zero fill from requested length,
-        # to make sure truncated data is not leaked
-        s = slice(length, MAX_SMALL)
-        self.host[self.root][s] = ZERO_BLOCK[s]
-        # no zero fill when growing, since we assume the block was
-        # zeroed either above or on blob creation
-      self.type = SMALL_PAGE
-      self.capacity = capacity
+        self.type = SMALL_PAGE
+      # already a small block
+      # zero fill from requested length,
+      # to make sure truncated data is not leaked
+      self.host[self.root][s] = ZERO_BLOCK[s]
+      # no zero fill when growing, since we assume the block was
+      # zeroed either above or on blob creation
+      self.length = length
       self.flush_root()
       return
 
     # requested size requires a regular blob
-    num_blocks = BLOCK(length + BLOCK_MASK)
+    num_blocks = (length + self.items_per_block - 1) // self.items_per_block
     cur_blocks = self.num_blocks
 
     if cur_blocks == num_blocks:
       # don't need to allocate or free any blocks
       # zero fill any truncated space
-      i = BLOCK(length)
+      i = (length - 1) // self.items_per_block
       if i != cur_blocks:
         b = self.get_block(i)
-        s = slice(OFFSET(length), BLOCK_SIZE)
+        # TODO use length:self.length, not whole block
+        s = slice(length % self.items_per_block, BLOCK_SIZE)
         b[s] = ZERO_BLOCK[s]
-      self.capacity = capacity
+      self.length = length
       self.flush_root()
       return
 
-    data = None
-    l = self.length
     if self.type == SMALL_PAGE:
-      data = bytes(self.host[self.root][0:self.length * self.item_size])
-      self.type = REGULAR_PAGE
-      self.length = 0
-      self.capacity = 0
-      # def _pop():
-      #   return data.pop()
-      # self.pop = _pop
+      self.make_regular()
 
     # allocate/free blocks
     # may call append/pop!
     self.allocate(num_blocks)
 
-
-    # copy data back from small block expansion
-    if data:
-      self.get_block(0)[0:len(data)] = data
-      self.length = l
-      # del self.pop
-
-    # still need this because we may have over-allocated
-    # if length was not on a page boundary
-    self.capacity = capacity
+    self.length = length
     self.flush_root()
+
+  def __repr__(self):
+    return '''<Array(root=%d, type=%d, format=%s, num_blocks=%d, length=%d)>''' % \
+        (self.root, self.type, self.format, self.num_blocks, self.length)
--- tests/test_array.py
+++ tests/test_array.py
@@ -6,21 +6,122 @@ import everdb
 
 TEST_NAME = 'test_archive.deleteme.dat'
 
+
+def test_resize():
+  db = everdb.Database(TEST_NAME, overwrite=True)
+  db.freelist = []
+  ar = db.array('I')
+
+  capacity = (4096 - 18) // 4
+  for i in range(capacity - 1):
+    ar.append(i)
+    assert ar[i] == i
+    assert ar.type == 1
+    assert ar.length == i + 1
+    assert ar.capacity == capacity
+    with pytest.raises(IndexError):
+      j = ar[i + 1]
+
+  # small to regular block allocation
+  ar.append(4)
+  assert ar[capacity - 1] == 4
+  assert ar.type == 2
+  assert ar.length == capacity
+  assert ar.capacity == 4096 // 4
+  with pytest.raises(IndexError):
+    j = ar[capacity]
+
+
+  capacity = 4096 // 4
+  for i in range(ar.length, capacity - 1):
+    ar.append(i)
+    assert ar[i] == i
+    assert ar.type == 2
+    assert ar.length == i + 1
+    assert ar.capacity == capacity
+    with pytest.raises(IndexError):
+      j = ar[i + 1]
+
+  # 1 to 2 block allocation
+  ar.append(5)
+  assert ar[capacity - 1] == 5
+  assert ar.type == 2
+  assert ar.length == capacity
+  assert ar.capacity == 2 * 4096 // 4
+  with pytest.raises(IndexError):
+    j = ar[capacity]
+
+  # 2 block indexing
+  capacity = 2 * 4096 // 4
+  for i in range(ar.length, capacity - 1):
+    ar.append(i)
+    assert ar[i] == i
+    assert ar.type == 2
+    assert ar.length == i + 1
+    assert ar.capacity == capacity
+    with pytest.raises(IndexError):
+      j = ar[i + 1]
+
+  # going down
+
+  for i in range(ar.length - 1, 4096 // 4 - 1, -1):
+    assert ar.pop() == i
+    assert ar.type == 2
+    assert ar.length == i
+    assert ar.capacity == capacity
+    with pytest.raises(IndexError):
+      j = ar[i]
+
+  # item that caused 1->2 allocation
+  assert ar.pop() == 5
+  assert ar.length == 1023
+  assert ar.capacity == 2 * 4096 // 4
+
+  # prev item should cause 2->1 shrink
+  assert ar.pop() == 1022
+  assert ar.length == 1022
+  assert ar.capacity == 4096 // 4
+
+  capacity = 4096 // 4
+  for i in range(ar.length - 1, (4096 - 18) // 4 - 1, -1):
+    assert ar.pop() == i
+    assert ar.type == 2
+    assert ar.length == i
+    assert ar.capacity == capacity
+    with pytest.raises(IndexError):
+      j = ar[i]
+
+  # item that caused 0->1 allocation
+  assert ar.pop() == 4
+  assert ar.type == 2
+  assert ar.length == 1018
+  assert ar.capacity == capacity
+
+  # prev item should cause 1->0 allocation
+  assert ar.pop() == 1017
+  assert ar.length == 1017
+  assert ar.type == 1
+  assert ar.capacity == (4096 - 18) // 4
+
+  capacity = (4096 - 18) // 4
+  for i in range(ar.length - 1, -1, -1):
+    assert ar.pop() == i
+    assert ar.type == 1
+    assert ar.length == i
+    assert ar.capacity == capacity
+    with pytest.raises(IndexError):
+      j = ar[i]
+
 def test_array():
   db = everdb.Database(TEST_NAME, overwrite=True)
-  # db.freelist = []
+  db.freelist = []
   ar = db.array('I')
   r = ar.root
 
+  assert everdb.Array._header_size == 18
+
   with pytest.raises(IndexError):
     x = ar.pop()
-
-  with pytest.raises(NotImplementedError):
-    x = ar[0:1]
-
-  with pytest.raises(NotImplementedError):
-    ar[0:1] = [1]
-
 
   N = 2000
   for i in range(N):
@@ -76,7 +177,7 @@ def test_array():
 
 def test_todo():
   db = everdb.Database(TEST_NAME, overwrite=True)
-  # db.freelist = []
+  db.freelist = []
   ar = db.array('I')
   r = ar.root
 
